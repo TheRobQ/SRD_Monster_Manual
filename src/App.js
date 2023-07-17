07f6c76a91f39389aacf6dc2a08d253278caa8e1
therobq <robert.quan@dts-global.com>
2023-07-17 17:21:53 -0600

    project start swith data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Monsterlist from './components/monsterlist/Monsterlist';
 import './App.css';
 
+
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+  
+  useEffect(()=>{
+    getData()
+  },[]);
+
+  const getData = async () => {
+    try{
+      let response = await fetch('data.json'
+      ,{
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      } )
+      let data = await response.json();
+      setMonsters(data.monsters);
+    } catch( error ) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Monsterlist monsters = {monsters} />
     </div>
   );
 }
